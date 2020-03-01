340271a974a99dbc404cd85c66d920b46d23edd4
Thodze <doanthodtvt@gmail.com>
2020-03-01 21:31:23 +0700

    updated migration

--- app/Question.php
+++ app/Question.php
@@ -3,8 +3,21 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\User;
+use App\Reply;
+use App\Category;
 
 class Question extends Model
 {
-    //
+    public function users() {
+        return $this->belongsTo(User::class);
+    }
+
+    public function relies() {
+        return $this->hasMany(Reply::class);
+    }
+
+    public function categories() {
+        return $this->belongsTo(Category::class);
+    }
 }
--- app/Reply.php
+++ app/Reply.php
@@ -3,8 +3,16 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Question;
+use App\User;
 
 class Reply extends Model
 {
-    //
+    public function questions() {
+        return $this->belongsTo(Question::class);
+    }
+
+    public function users() {
+        return $this->belongsTo(User::class);
+    }
 }
